d4e1e907063d2afc26f2a3f783af5d930db26946
fannyzhl <fannyzhl583@gmail.com>
2020-11-22 22:00:05 -0400

    cambios en el pool

--- app/db/dev/pool.js
+++ app/db/dev/pool.js
@@ -1,14 +1,18 @@
 const pg = require("pg");
 const dotenv = require("dotenv");
 const { Pool } = pg;
+const url = require("url");
 dotenv.config();
 
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(":");
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split("/")[1],
 });
 
 module.exports = pool;
